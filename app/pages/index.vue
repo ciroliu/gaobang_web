cb5ab8f1a99f662b5320c1032ea9307a99175a32
liushouping <kose146352@gmail.com>
2025-09-05 19:34:51 +0800

    test

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,9 +3,10 @@
     class="relative overflow-hidden">
         <!--  -->
         <div class="all-colors">
-            <div class="color color-orange fixed left-8 bottom-8 -z-10 w-[450px] h-[450px] blur-[180px]"></div>
-            <div class="color color-yellow fixed right-8 bottom-8 -z-10 w-[450px] h-[450px] blur-[70px]"></div>
-            <div class="color color-pink fixed left-[8px] lg:left-[450px] top-2 -z-10 w-[450px] h-[450px] blur-[180px]"></div>
+            <div class="color color-orange fixed left-8 bottom-8 -z-10 w-[600px] h-[600px] blur-[120px]"></div>
+            <div class="color color-pink fixed right-8 -bottom-32 -z-10 w-[600px] h-[600px] blur-[120px]"></div>
+            <div class="color color-orange fixed right-8 bottom-10 -z-10 w-[800px] h-[800px] blur-[120px]"></div>
+            <div class="color color-pink fixed left-[8px] lg:left-[450px] top-2 -z-10 w-[600px] h-[600px] blur-[120px]"></div>
         </div>
         <AppHeader />
         <!--  -->
