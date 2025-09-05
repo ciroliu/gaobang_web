86ce09fe305c6f0dba1dcfe919778a4b5976e9af
liushouping <kose146352@gmail.com>
2025-09-06 07:59:15 +0800

    test

--- app/app.vue
+++ app/app.vue
@@ -2,8 +2,14 @@
 useSeoMeta({
   titleTemplate: '高邦創意 GAOBANG',
   description: '高邦創意專注企劃策展、主題活動，擅長結合動漫IP規劃，發揮IP魅力與主題活動的跨界結合，打造加倍的快樂魅力。',
-  ogImage: '/og-gaobang.png',
+
+  ogTitle: '高邦創意 GAOBANG',
+  ogDescription: '高邦創意專注企劃策展、主題活動，擅長結合動漫IP規劃，發揮IP魅力與主題活動的跨界結合，打造加倍的快樂魅力。',
+  ogImage: 'https:/gaobang.art/og-gaobang.png',
   ogImageAlt: '高邦創意',
+  ogUrl: 'https://gaobang.art',
+  ogType: 'website',
+
 });
 </script>
 
