80448d7ac5f11b32c65be7543e10d83bac56d1cc
liushouping <kose146352@gmail.com>
2025-09-06 08:30:29 +0800

    test

--- app/components/whatwedid.vue
+++ app/components/whatwedid.vue
@@ -112,13 +112,13 @@
         </button>
 
         <div class="mt-6 mb-4">
-          <span class="bg-[#242870] text-white text-[12px] rounded-xl px-6 py-1">2019/07/05 – 2019/09/22</span>
+          <span class="bg-[#242870] text-white text-[12px] rounded-xl px-6 py-1 font-GenJyuuGothicRegular">2019/07/05 – 2019/09/22</span>
         </div>
         <div class="w-full flex flex-col xl:flex-row justify-center items-start gap-6 px-2 pb-10">
           <div class="w-full xl:w-1/3">
-            <h2 class="text-[24px] font-bold mb-10">2019 航海王動畫20週年紀念特展</h2>
-            <p class="text-gray-700 text-[12px] mb-4">海內外超過百則媒體報導，吸引超過10萬名海賊迷朝聖！主辦於台灣台北「松山文創園區4、5號」倉庫，展期為期近三個月長。</p>
-            <p class="text-gray-700 text-[12px]">首次結合「五感體驗」，以知覺、視覺、聽覺、觸覺及味覺做完美結合，首度展場加入輕食區，超越以往展覽規模，24隻大型公仔現身，其中更有 Big Mom 425公分高讓粉絲驚嘆不已，更為台灣首次曝光 ！展期導入 AR 互動及電子集章活動，讓粉絲能夠在回味劇情，還能留下紀念。</p>
+            <h2 class="text-[24px] font-bold mb-10 font-GenJyuuGothicRegular">2019 航海王動畫20週年紀念特展</h2>
+            <p class="text-gray-700 text-[12px] mb-4 font-GenJyuuGothicRegular">海內外超過百則媒體報導，吸引超過10萬名海賊迷朝聖！主辦於台灣台北「松山文創園區4、5號」倉庫，展期為期近三個月長。</p>
+            <p class="text-gray-700 text-[12px] font-GenJyuuGothicRegular">首次結合「五感體驗」，以知覺、視覺、聽覺、觸覺及味覺做完美結合，首度展場加入輕食區，超越以往展覽規模，24隻大型公仔現身，其中更有 Big Mom 425公分高讓粉絲驚嘆不已，更為台灣首次曝光 ！展期導入 AR 互動及電子集章活動，讓粉絲能夠在回味劇情，還能留下紀念。</p>
           </div>
           <div class="w-full xl:w-2/3">
             <img src="/whatdid.webp" class="w-full">
