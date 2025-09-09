b6004108e5b957f0ef59e9c04bc3510486ba566d
liushouping <kose146352@gmail.com>
2025-09-09 23:52:22 +0800

    test

--- app/error.vue
+++ app/error.vue
@@ -20,7 +20,7 @@
             style="text-indent: 0em;">Page Not Found.</p>
             <a 
             href="#" 
-            class="border border-2 rounded-[30px] px-6 py-1 text-white  font-GenJyuuGothicBold z-50 mt-6"
+            class="border border-2 rounded-[30px] px-6 py-1 text-white font-GenJyuuGothicRegular z-50 mt-6 hover:bg-white hover:text-[#ED0081]"
             @click.prevent="handleError">回到首頁</a>
     </section>
 </template>
