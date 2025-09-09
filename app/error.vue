0860c9d962b0aef3eda4af210fb9de4bdbedb866
liushouping <kose146352@gmail.com>
2025-09-10 00:00:52 +0800

    test

--- app/error.vue
+++ app/error.vue
@@ -1,14 +1,18 @@
 <template>
-
     <section 
     class="relative bg-gradient-to-t from-[#ED0081] to-[#F13630] w-full h-screen flex flex-col justify-center items-center z-20 overflow-hidden">
-
       <div class="all-colors">
             <div class="color color-orange absolute left-8 bottom-8 w-[600px] h-[600px] blur-[180px]"></div>
             <div class="color color-pink absolute right-8 -bottom-32 w-[600px] h-[600px] blur-[180px]"></div>
             <div class="color color-orange absolute right-8 bottom-10 w-[800px] h-[800px] blur-[180px]"></div>
             <div class="color color-pink absolute left-[8px] lg:left-[450px] top-2 w-[600px] h-[600px] blur-[180px]"></div>
       </div>
+            <div class="fixed top-6 right-6">
+              <img 
+              class="w-[60px]"
+              src="/logo.png" 
+              alt="logo">
+            </div>
             <h5 
             style="rotate: 5deg;"
             class="text-white font-GenJyuuGothicBold tracking-[1px] text-[128px] z-50">404</h5>
